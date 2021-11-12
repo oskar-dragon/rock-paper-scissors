a523a704308b7efe8a8c8bb0eeebfba82940ba9a
Oskar Dragon <oskartdragon@gmail.com>
2021-11-12 16:49:53 +0000

    Refactored code, simplified functions

--- index.pack.js
+++ index.pack.js
@@ -10,6 +10,13 @@ const textElement = document.querySelector(".text");
 
 let playerScore = 0;
 let computerScore = 0;
+
+const bgcolor = {
+  green: "#a6caa6",
+  red: "#d66969",
+  yellow: "#DECD92",
+  grey: "#8e8e8e",
+};
 const rpsOptions = [
   {
     name: "rock",
@@ -40,7 +47,6 @@ startBtn.addEventListener("click", () => {
 rockBtn.addEventListener("click", rockPaperScissors);
 paperBtn.addEventListener("click", rockPaperScissors);
 scissorsBtn.addEventListener("click", rockPaperScissors);
-
 startAgainBtn.addEventListener("click", startAgain);
 
 function rockPaperScissors(e) {
@@ -70,67 +76,62 @@ function rockPaperScissors(e) {
 function playerWin() {
   playerScore++;
   playerScoreElement.innerHTML = `${playerScore}`;
-  document.body.style.backgroundColor = "#a6caa6";
-  startAgainBtn.style.backgroundColor = "#a6caa6";
+  document.body.style.backgroundColor = bgcolor.green;
+  startAgainBtn.style.backgroundColor = bgcolor.green;
 
   if (playerScore === 3) {
     textElement.innerHTML = "You have won!";
-    rockBtn.style.pointerEvents = "none";
-    paperBtn.style.pointerEvents = "none";
-    scissorsBtn.style.pointerEvents = "none";
-    startAgainBtn.classList.remove("hide");
-  } else if (computerScore === 3) {
-    textElement.innerHTML = "You have lost!";
-    rockBtn.style.pointerEvents = "none";
-    paperBtn.style.pointerEvents = "none";
-    scissorsBtn.style.pointerEvents = "none";
+    disablePointerEvents();
     startAgainBtn.classList.remove("hide");
   } else {
     textElement.innerHTML = "Point for you!";
-    startAgainBtn.classList.add("hide");
   }
 }
 
 function computerWin() {
   computerScore++;
   computerScoreElement.innerHTML = `${computerScore}`;
-  document.body.style.backgroundColor = "#d66969";
-  startAgainBtn.style.backgroundColor = "#d66969";
+  document.body.style.backgroundColor = bgcolor.red;
+  startAgainBtn.style.backgroundColor = bgcolor.red;
 
-  if (playerScore === 3) {
-    textElement.innerHTML = "You have won!";
-    rockBtn.style.pointerEvents = "none";
-    paperBtn.style.pointerEvents = "none";
-    scissorsBtn.style.pointerEvents = "none";
-    startAgainBtn.classList.remove("hide");
-  } else if (computerScore === 3) {
-    textElement.innerHTML = "You have lost!";
-    rockBtn.style.pointerEvents = "none";
-    paperBtn.style.pointerEvents = "none";
-    scissorsBtn.style.pointerEvents = "none";
+  if (computerScore === 3) {
+    textElement.innerHTML = "Computer has won!";
+    disablePointerEvents();
     startAgainBtn.classList.remove("hide");
   } else {
-    textElement.innerHTML = "Point for compter!";
-    startAgainBtn.classList.add("hide");
+    textElement.innerHTML = "Point for computer!";
   }
 }
 
 function draw() {
   textElement.innerHTML = "Draw!";
-  document.body.style.backgroundColor = "#DECD92";
+  document.body.style.backgroundColor = bgcolor.yellow;
   startAgainBtn.classList.add("hide");
 }
 
 function startAgain() {
-  computerEmoji.innerHTML = rpsOptions[3].emoji;
-  document.body.style.backgroundColor = "#8e8e8e";
-  playerScore = 0;
+  document.body.style.backgroundColor = bgcolor.grey;
+  resetScore();
   playerScoreElement.innerHTML = `${playerScore}`;
-  computerScore = 0;
   computerScoreElement.innerHTML = `${computerScore}`;
   textElement.innerHTML = "Rock Paper Scissors?";
   startAgainBtn.classList.add("hide");
+  enablePointerEvents();
+}
+
+function disablePointerEvents() {
+  rockBtn.style.pointerEvents = "none";
+  paperBtn.style.pointerEvents = "none";
+  scissorsBtn.style.pointerEvents = "none";
+}
+
+function enablePointerEvents() {
   rockBtn.style.pointerEvents = "auto";
   paperBtn.style.pointerEvents = "auto";
   scissorsBtn.style.pointerEvents = "auto";
+}
+
+function resetScore() {
+  playerScore = 0;
+  computerScore = 0;
 }
--- readme.md
+++ readme.md
@@ -1,13 +1,14 @@
-# Rock Paper Scissors Game
+# Project Name
 
 > A simple Rock Paper Scissors game that everyone knows!
-> Live demo [_here_](https://www.example.com). <!-- If you have the project hosted somewhere, include the link here. -->
+> Live demo [_here_](https://oskar-dragon.github.io/rock-paper-scissors/)
 
 ## Table of Contents
 
-- [General Info](#general-information)
+- [General Information](#general-information)
+- [Screenshots](#screenshots)
 - [Project Status](#project-status)
-- [Room for Improvement](#room-for-improvement)
+- [Author Info](#author-info)
 
 ## General Information
 
@@ -15,23 +16,24 @@ I've created this simple Rock Paper Scissors game where two players face each ot
 
 Each sign can beat one (and only one) other:
 
-- Rock smashes scissors
-- Scissors cut paper
-- Paper covers rock
-- If both players choose the same sign, the game is a draw
-
-The user has to click on of the buttons that represent one of the options (Rock, Paper, Scissors). Once user clicks the button, a computer randomly choosees one of these options too. The winner needs to have 3 points to win.
+Rock smashes scissors
+Scissors cut paper
+Paper covers rock
+If both players choose the same sign, the game is a draw
+The user has to click on of the buttons that represent one of the options (Rock, Paper, Scissors). Once user clicks the button, a computer randomly chooses one of these options too. The winner needs to have 3 points to win.
 
 The purpose of this project was to practice JavaScript skills before I will attempt to learn APIs. Games are fun way to practice creating a logic.
 
+## Screenshots
+
+![Example screenshot](https://i.gyazo.com/e3423fb3b494967842f26a334c6ddd61.png)
+
 ## Project Status
 
-Project is: _complete_
+Project is: _complete_.
 
-## Room for Improvement
+## Author Info
 
-Include areas you believe need improvement / could be improved. Also add TODOs for future development.
-
-Room for improvement:
-
-- Refactor some functions to make them ever shorter/simpler
+Created by [Oskar Dragon](https://github.com/oskar-dragon) |
+[LinkedIn](https://www.linkedin.com/in/oskar-dragon) |
+[Instagram](https://www.instagram.com/skrdrgn___/)
